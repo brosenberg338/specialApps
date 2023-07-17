611380dcc43920f905725f53a304b1bbd1900577
barry <barry1468@gmail.com>
2023-07-16 22:27:42 -0400

    recipes

--- recipes/routes/allergies.js
+++ recipes/routes/allergies.js
@@ -19,18 +19,19 @@ router.post('/', function (req, res, next) {
   let allergy2 = req.body.allergy2 || '';
   let allergy3 = req.body.allergy3 || '';
 
- // connection.query(SQL` SELECT *  FROM recipe `, function (error, results, fields) {
-//  connection.query('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = recipe ' , function (error, results, fields) {
-   // connection.query("SELECT * FROM recipe WHERE ingredient1 NOT IN 'fish' " , function (error, results, fields) {
  
-   connection.query(SQL` SELECT *  FROM recipe WHERE NOT ingredient1 = ${allergy1} &&  NOT ingredient1 = ${allergy2} 
+    connection.query(SQL` SELECT *  FROM recipe WHERE NOT ingredient1 = ${allergy1} &&  NOT ingredient1 = ${allergy2} 
     &&  NOT ingredient1 = ${allergy3} && NOT ingredient2 = ${allergy1} &&  NOT ingredient2 = ${allergy2} 
     &&  NOT ingredient2 = ${allergy3} && NOT ingredient3 = ${allergy1} &&  NOT ingredient3 = ${allergy2} 
-    &&  NOT ingredient3 = ${allergy3} && NOT ingredient4 = ${allergy1} &&  NOT ingredient4 = ${allergy2} 
-    &&  NOT ingredient4 = ${allergy3}`, function (error, results, fields)   {
+    &&  NOT ingredient3 = ${allergy3} && NOT ingredient4 = ${allergy1} &&  NOT ingredient4 = ${allergy2}  
+    &&  NOT ingredient4 = ${allergy3}`, function (error, results, fields)   { 
+     /* connection.query(SQL` SELECT *  FROM recipe WHERE NOT ingredient1 = ${allergy1} || ${allergy2} || ${allergy3} && NOT ingredient2 = ${allergy1} || ${allergy2} || ${allergy3} && NOT ingredient3 = ${allergy1} || ${allergy2} || ${allergy3} && NOT ingredient4 = ${allergy1} || ${allergy2} || ${allergy3}`, function (error, results, fields)   {       */
+    
+     /* connection.query(SQL`SELECT *  FROM recipe WHERE NOT ingredient1 = ${allergy2}  ||   ${allergy1}  && NOT ingredient4 = ${allergy2}  && NOT  ingredient3 = ${allergy1}  `) , function (error, results, fields)   { */
+     
   if (error) throw error;
     
-
+  
   res.render('recipes', {
     title: 'Recipes',
     recipes: results
